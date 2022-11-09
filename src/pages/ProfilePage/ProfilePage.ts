a31a83014373b05e9d0b67fb0446a34000159388
Krivonosov Artem Albertovich <aakrivonosov@sberbank.ru>
2022-11-09 11:14:37 +0300

    Project work 3nd sprint clear code

--- src/pages/ProfilePage/ProfilePage.ts
+++ src/pages/ProfilePage/ProfilePage.ts
@@ -8,15 +8,6 @@ import Avatar from '../../components/Avatar/Avatar';
 
 const router = new Router('root');
 
-// const templateProfileData = [
-//     { item: 'Почта', info: 'mail@ya.ru', id: 'email' },
-//     { item: 'Логин', info: 'login', id: 'login' },
-//     { item: 'Имя', info: 'Артем', id: 'first_name' },
-//     { item: 'Фамилия', info: 'Артемов', id: 'last_name' },
-//     { item: 'Имя в чате', info: 'Artem_1', id: 'display_name' },
-//     { item: 'Телефон', info: '+7 (777) 777 77 77', id: 'phone' },
-// ];
-
 UserController.getUser();
 
 const templateProfileData = [
@@ -36,10 +27,6 @@ const templateProfileData = [
         id: 'phone',
     },
 ];
-
-// const templateProfile = templateProfileData.map((elem) =>
-//     connect((state) => ({ item: elem.item, info: elem.info, id: state.user?.login })),
-// );
 
 const profileData = templateProfileData.map((i) => {
     const A = connect((state) => ({
