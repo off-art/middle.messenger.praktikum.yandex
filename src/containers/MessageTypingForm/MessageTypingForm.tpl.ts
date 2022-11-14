a9ae0c5ed7d04e906445955cb5b825a62a5e4009
Krivonosov Artem Albertovich <aakrivonosov@sberbank.ru>
2022-11-14 10:55:49 +0300

    Post-review fixes

--- src/containers/MessageTypingForm/MessageTypingForm.tpl.ts
+++ src/containers/MessageTypingForm/MessageTypingForm.tpl.ts
@@ -1,10 +1,8 @@
-export const tpl = `        
-            <div class='message-typing-form-submit message-typing-form-staple'></div>
+export const tpl = `<div class='message-typing-form-submit message-typing-form-staple'></div>
             <input
                 class='message-typing-form-input'
                 placeholder='Сообщение'
                 data-required='true'
                 id='message'
             />
-            <input type="button" class='message-typing-form-submit' value='➜'>      
-`;
+            <button class='message-typing-form-submit'>➜</button>`;
